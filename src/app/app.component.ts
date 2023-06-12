055b13465805236309f7442c2318d2eae501dbe5
721678 <marshallmac2@yahoo.com>
2023-06-12 18:44:57 -0400

    factory app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'factoryshop';
+  title = 'ecommerce';
+  //Sidebar toggle show hide function
+  status = false;
+  addToggle()
+  {
+    this.status = !this.status;       
+  }
 }
